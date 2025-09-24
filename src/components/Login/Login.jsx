a3e7c99a1b3d5073c65c2f5570dab54c105126b6
Sebastian Peñaranda <penarandagrijalbasebastian@gmail.com>
2025-09-23 23:55:17 -0500

    Vercel

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
-import React from "react";
-=======
 
->>>>>>> 42be63683b4a48f9200536eaf767bb48b9b04df1
+import React from "react";
+
+
 import LoginManager from "./LoginManager";
 import "./Login.css";
 
